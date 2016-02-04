080493b985346bc042b87c6aeaf3790bf1fc283b
Roy Gerritse <info@roygerritse.nl>
2016-02-05 00:18:58 +0100

    Initial commit for oAuth AngularJS project.

---
+++ RoyGerritse.API/AuthContext.cs
@@ -0,0 +1,19 @@
+﻿using RoyGerritse.API.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace RoyGerritse.API
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+     
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+
+}
---
+++ RoyGerritse.API/Models/Enums.cs
@@ -0,0 +1,9 @@
+﻿namespace RoyGerritse.API.Models
+{
+    public enum ApplicationTypes
+    {
+        JavaScript = 0,
+        NativeConfidential = 1
+    };
+    
+}
